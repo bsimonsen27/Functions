3130f507fe64df66d64df06e125328401db55827
Brendon Simonsen <brendonsimonsen@mail.weber.edu>
2021-04-05 08:40:59 -0600

    Initial Commit

---
+++ calculator.py
@@ -0,0 +1,6 @@
+# get information from the user about conversion
+userInput = input('What is the number?')
+print('Your number is :', userInput)
+
+# perform the conversion 
+# print that information
