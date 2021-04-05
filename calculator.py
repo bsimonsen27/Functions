7ae5c1d3ee556cb1e4699e81c3971a263d523288
Brendon Simonsen <brendonsimonsen@mail.weber.edu>
2021-04-05 08:54:33 -0600

    initial calculations

--- calculator.py
+++ calculator.py
@@ -1,6 +1,16 @@
 # get information from the user about conversion
-userInput = input('What is the number?')
-print('Your number is :', userInput)
+# TODO user input validation 
+userInput = input('What is the number? ')
+print('Your number is :', userInput) # TODO remove 
+userNumber = float(userInput)
 
 # perform the conversion 
+# convert in to mm 
+# convert from in to mm (userNumber * 25.4)
+userAnswer = userNumber * 25.4
+
+# convert from mm to in (userNumber / 25.4)
+userAnswer = userNumber / 25.4
+
 # print that information
+print('The answer is', userAnswer)
