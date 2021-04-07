b21fe391a9cae9804b63fc4c2cd3a074430766bd
Brendon Simonsen <brendonsimonsen@mail.weber.edu>
2021-04-07 09:09:25 -0600

    added while loop

--- calculator.py
+++ calculator.py
@@ -2,35 +2,43 @@
 # TODO user input validation for menu
 #TODO user input validation fro menu
 #user input on the type of conversion
-userConversion = input('What type of conversion?\n\t1-inches to mm.\n\t2- mm to inches.\n')
-
-userInput = input('What is the number? ')
-print('Your number is :', userInput) # TODO remove 
-userNumber = float(userInput)
-
-#function that calculates
 def in2mm(userNumber):
-    userAnswer = userNumber * 25.4
-    return userAnswer
+        return userNumber * 25.4
 
 def mm2in(userNumber):
-    userAnswer = userNumber / 25.4
-    return userAnswer
+    return userNumber / 25.4
 
-def calculate(userNumber, converstionType):
-    if converstionType == '1':
-        userAnswer = userNumber * 25.4
-    if converstionType == '2':
-        userAnswer = userNumber / 25.4
-    return userAnswer
+def in2ft(userNumber):
+    return userNumber / 12
 
-calcVal = calculate(userNumber, userConversion)
 
-def printResults(type, input, calc):
+def printResults(type, input):
+    #calcVal = calculate(userNumber, userConversion)
     if type == '1':
-        print('The answer is', input, 'in =', calc, 'mm')
+        cfrom = 'in'
+        to = 'mm'
+        calcVal = in2mm(input)
     if type == '2':
-        print('The answer is', input, 'mm =', calc, 'in')
+        cfrom = 'mm'
+        to = 'in'
+        calcVal = mm2in(input)
+    if type == '3':
+        cfrom = 'in'
+        to = 'ft'
+        calcVal = in2ft(input)
+    print('The answer is', input, cfrom,'=', calcVal, to)
 
-printResults(userConversion, userInput, calcVal)
+while True:
+    userConversion = input('What type of conversion?\n\t1-inches to mm.\n\t2- mm to inches.\n\t3-inches to feet.\n\tq to quit.\n')
+    if userConversion == 'q':
+        break
+    userInput = input('What is the number? ')
+    print('Your number is :', userInput) # TODO remove 
+    userNumber = float(userInput)
+
+    #function that calculates
+    
+
+
+    printResults(userConversion, userNumber)
 
